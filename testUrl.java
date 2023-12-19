9d5b3f2d90d0f68de637edc5fa9201949d8d37d9
Wei Yin Shing <125491363+niyiewgnihs@users.noreply.github.com>
2023-12-19 12:05:01 +0100

    Removed "Basic" as Authorization

--- testUrl.java
+++ testUrl.java
@@ -5,26 +5,23 @@ import java.net.URL;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-public class testUrl {
+public class LumiereGetRequest {
+    final static String TOKEN = "Token 7d29e9dc5e427bf56ab9f783e7f015ffef6258e7";
+
     public static void main(String[] args) {
-       
+
         try{
-          int limit = 10;
-          int offset = 0;
-          final String TOKEN = "Token 7d29e9dc5e427bf56ab9f783e7f015ffef6258e7";
-          String apiUrl = "http://dronesim.facets-labs.com/api/dronedynamics/";
-          String urlWithParams = apiUrl + "?limit=" + limit + "&offset=" + offset;
-          URL url = new URL (urlWithParams) ;
-          HttpURLConnection con = (HttpURLConnection) url.openConnection();
-          con.setRequestProperty("Authorization", "Basic" + TOKEN);
-          con.setRequestMethod("GET");
-          //con.setRequestProperty ("group27", "certainty-garden-wok-epiphany-judiciary-bacterium-entomb") ;
-          int responseCode = con.getResponseCode () ;
-          System.out.println(responseCode); 
+            URL url = new URL("http://dronesim.facets-labs.com/api/drones/?limit=10&offset=0");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("Authorization", TOKEN);
+            con.setRequestMethod("GET");
+            //con.setRequestProperty ("group27", "certainty-garden-wok-epiphany-judiciary-bacterium-entomb") ;
+            int responseCode = con.getResponseCode () ;
+            System.out.println(responseCode);
 
         }catch (Exception e) {
-         // Behandlung der Ausnahme
-         e.printStackTrace(); 
-       }  
+            // Behandlung der Ausnahme
+            e.printStackTrace();
+        }
     }
 }
